c19244e86c84d4dd7e61ec4b4181482ef963e5de
felipeoli7eira <me.felipeoliveira@gmail.com>
2020-12-12 17:53:16 -0300

    reponse to the index route

--- controllers/Home.js
+++ controllers/Home.js
@@ -5,11 +5,33 @@ class Home
     {
         res.json(
             {
-                message: "Welcome! to use this application, open an http client and send requests to the endpoints listed in the endpoints key",
+                message: "Welcome! to use this app, open an http client and send requests to the endpoints listed in the endpoints key",
                 endpoints: [
                     {
                         "verb": "GET",
                         "route": "/users"
+                    },
+
+                    {
+                        "verb": "POST",
+                        "route": "/users",
+                        "body": ["name", "email", "password", "role"]
+                    },
+
+                    {
+                        "verb": "GET",
+                        "route": "/user/:id"
+                    },
+
+                    {
+                        "verb": "PUT",
+                        "route": "/user",
+                        "body": ["name", "email", "role"]
+                    },
+
+                    {
+                        "verb": "DELETE",
+                        "route": "/user/:id"
                     }
                 ]
             }
